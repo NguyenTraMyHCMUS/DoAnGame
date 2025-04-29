bb45d11ed74d598089238339a59c41a597e2e349
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-04-29 12:57:34 +0700

    Update and rename BlockFactory.cpp to TetrominoFactory.cpp
    Sửa tên và toàn bộ code để phù hợp với Game quản lí

---
+++ source/src/TetrominoFactory.cpp
@@ -0,0 +1,19 @@
+#include "include/TetrominoFactory.h"
+
+std::unique_ptr<Tetromino> TetrominoFactory::createRandomTetromino() {
+    int type = rand() % 7; // Chọn ngẫu nhiên một loại khối
+    return createTetromino(type);
+}
+
+std::unique_ptr<Tetromino> TetrominoFactory::createTetromino(int type) {
+    switch (type) {
+        case 0: return std::make_unique<ITetromino>();
+        case 1: return std::make_unique<JTetromino>();
+        case 2: return std::make_unique<LTetromino>();
+        case 3: return std::make_unique<OTetromino>();
+        case 4: return std::make_unique<STetromino>();
+        case 5: return std::make_unique<TTetromino>();
+        case 6: return std::make_unique<ZTetromino>();
+        default: return nullptr;
+    }
+}
