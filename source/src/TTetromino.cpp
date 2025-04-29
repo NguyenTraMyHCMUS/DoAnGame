29bf2a4ef0edd131fbc3bee2c0e7a05d13fb19d7
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-04-29 12:52:12 +0700

    Update and rename TBlock.cpp to TTetromino.cpp
    Sửa tên và toàn bộ code để phù hợp với Game quản lí

---
+++ source/src/TTetromino.cpp
@@ -0,0 +1,14 @@
+#include "TTetromino.h"
+
+TTetromino::TTetromino() {
+    _color = 4; // Màu sắc cho khối T
+    initializeShape(); // Khởi tạo hình dạng khối
+}
+
+void TTetromino::initializeShape() {
+    int figures[4] = {3, 5, 4, 7}; // Hình dạng khối T
+    for (int i = 0; i < 4; i++) {
+        _blocks[i]._x = figures[i] % 2;
+        _blocks[i]._y = figures[i] / 2;
+    }
+}
