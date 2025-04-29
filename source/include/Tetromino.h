cdccaca9da47a4a4b6dcc665395f2bce02299acf
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-04-29 13:01:26 +0700

    Update and rename Block.h to Tetromino.h

---
+++ source/include/Tetromino.h
@@ -0,0 +1,36 @@
+#ifndef TETROMINO_H
+#define TETROMINO_H
+
+#include "Field.h"
+#include <SFML/Graphics.hpp>
+
+struct Point {
+    int _x; // Tọa độ x của ô trong lưới
+    int _y; // Tọa độ y của ô trong lưới
+    Point(int x = 0, int y = 0) : _x(x), _y(y) {} // Constructor khởi tạo tọa độ
+};
+
+class Tetromino {
+protected:
+    Point _blocks[4]; // Mảng chứa 4 ô của khối Tetris
+    Point _backup[4]; // Lưu trạng thái khối để khôi phục
+    int _color; // Màu sắc của khối là cố định cho mỗi hình 
+    int _cellSize; // Kích thước của mỗi ô trong khối
+
+public:
+    Tetromino(); // Constructor khởi tạo khối Tetris
+    virtual ~Tetromino() = default; // Destructor ảo để giải phóng bộ nhớ
+
+    virtual void initializeShape() = 0; // Khởi tạo hình dạng khối Tetris
+    void setCellSize(int size); // Thay đổi kích thước ô trong khối
+    void backupState(); // Lưu trạng thái hiện tại của khối
+    void restoreState(); // Khôi phục trạng thái từ backup
+    void move(int dx); // Di chuyển khối theo chiều ngang
+    void rotate(); // Xoay khối
+    void fall(); // Di chuyển khối xuống dưới
+    bool isValid(const Field &field) const; // Kiểm tra vị trí hợp lệ
+    void lock(Field &field) const; // Cố định khối vào lưới
+    void draw(sf::RenderWindow &window, sf::Sprite &sprite) const; //  Vẽ khối lên cửa sổ trò chơi
+};
+
+#endif
