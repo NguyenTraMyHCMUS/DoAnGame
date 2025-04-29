95d24bba3f99d74b7196f2ff29197873eea3599c
NguyenTraMyHCMUS <23120300@student.hcmus.edu.vn>
2025-04-29 14:28:38 +0700

    Update Game.h

--- source/include/Game.h
+++ source/include/Game.h
@@ -1,27 +1,30 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include <SFML/Graphics.hpp>
-#include "Grid.h"
-#include "Block.h"
+#include "Field.h"
+#include "Tetromino.h"
+#include "ScoreManager.h"
+#include "TetrominoFactory.h"
 
 class Game {
+private:
+    RenderWindow window;
+    Texture t1, t2, t3;
+    Sprite s, background, frame;
+    Field field;
+    Tetromino tetromino;
+    Clock clock;
+    ScoreManager scoreManager;
+    float timer = 0, delay = 0.3;
+    int dx = 0;
+    bool rotate = false;
+
 public:
     Game();
+    void handleInput();
+    void update();
+    void draw();
     void run();
-
-private:
-    sf::RenderWindow window;
-    sf::Texture t1, t2, t3;
-    sf::Sprite s, background, frame;
-    Grid grid;
-    Block currentBlock;
-    float timer, delay;
-    int colorNum;
-
-    void processEvents();
-    void update(float deltaTime);
-    void render();
 };
 
 #endif
