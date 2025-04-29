a0a8fa20694a520d13150c08e82ad05402840ef1
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-04-29 16:26:12 +0700

    Update ITetromino.cpp

--- source/src/ITetromino.cpp
+++ source/src/ITetromino.cpp
@@ -12,3 +12,20 @@ void ITetromino::initializeShape() {
         _blocks[i]._y = figures[i] / 2;
     }
 }
+
+void ITetromino::rotate() {
+    static bool isHorizontal = true;
+    Point center = _blocks[1]; // Điểm trung tâm để xoay
+    if (isHorizontal) {
+        for (int i = 0; i < 4; i++) {
+            _blocks[i]._x = center._x;
+            _blocks[i]._y = center._y - 1 + i;
+        }
+    } else {
+        for (int i = 0; i < 4; i++) {
+            _blocks[i]._x = center._x - 1 + i;
+            _blocks[i]._y = center._y;
+        }
+    }
+    isHorizontal = !isHorizontal;
+}
