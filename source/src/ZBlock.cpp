139e926142bf46560256fa111c4fc70cce005819
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-04-29 12:52:37 +0700

    Update ZBlock.cpp
    Sửa tên và toàn bộ code để phù hợp với Game quản lí

--- source/src/ZBlock.cpp
+++ source/src/ZBlock.cpp
@@ -1,15 +1,14 @@
-#include "ZBlock.h"
+#include "ZTetromino.h"
 
-ZBlock::ZBlock() {
-    id = 7;
-    cells[0] = {{0, 0}, {1, 0}, {1, 1}, {2, 1}};  // Hình Z
-    cells[1] = {{1, -1}, {0, 0}, {1, 0}, {0, 1}};  // Xoay 90 độ
+ZTetromino::ZTetromino() {
+    _color = 2; // Màu sắc cho khối Z
+    initializeShape(); // Khởi tạo hình dạng khối
 }
 
-void ZBlock::Draw(int offsetX, int offsetY, sf::RenderWindow& window, sf::Sprite& sprite) {
-    sprite.setColor(sf::Color::Red);  // Đặt màu cho khối Z
-    for (auto& p : cells[rotationState]) {
-        sprite.setPosition((p.x + columnOffset + offsetX) * cellSize, (p.y + rowOffset + offsetY) * cellSize);
-        window.draw(sprite);  // Vẽ sprite lên cửa sổ
+void ZTetromino::initializeShape() {
+    int figures[4] = {2, 4, 5, 7}; // Hình dạng khối Z
+    for (int i = 0; i < 4; i++) {
+        _blocks[i]._x = figures[i] % 2;
+        _blocks[i]._y = figures[i] / 2;
     }
 }
