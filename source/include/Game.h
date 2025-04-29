a295105ddbd0fa9bcc09c781a773e74392234178
NguyenKhaNhuHCMUS <23120311@student.hcmus.edu.vn>
2025-04-29 20:56:52 +0700

    Update Game.h

--- source/include/Game.h
+++ source/include/Game.h
@@ -2,9 +2,10 @@
 #define GAME_H
 
 #include "Field.h"
-#include "Tetromino.h"
-#include "ScoreManager.h"
 #include "TetrominoFactory.h"
+#include "Tetromino.h"
+#include "ScoreManager"
+using namespace sf;
 
 class Game {
 private:
@@ -12,12 +13,15 @@ private:
     Texture t1, t2, t3;
     Sprite s, background, frame;
     Field field;
-    Tetromino tetromino;
+    std::unique_ptr<Tetromino> tetromino; // Sử dụng con trỏ thông minh để quản lý Tetromino
     Clock clock;
-    ScoreManager scoreManager;
     float timer = 0, delay = 0.3;
     int dx = 0;
     bool rotate = false;
+    bool isGameOver = false; // Biến để theo dõi trạng thái kết thúc trò chơi
+
+    int level = 1; // Cấp độ hiện tại
+    int linesCleared = 0; // Số dòng đã xóa
 
 public:
     Game();
