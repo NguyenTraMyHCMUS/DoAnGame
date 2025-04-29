8aa11c284ba4f240d9e527434f5123d3d4f08406
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-04-29 13:06:00 +0700

    Update and rename TBlock.h to TTetromino.h
    Sửa tên và toàn bộ code để phù hợp với Game quản lí

---
+++ source/include/TTetromino.h
@@ -0,0 +1,13 @@
+#ifndef TTETROMINO_H
+#define TTETROMINO_H
+
+#include "Tetromino.h"
+
+class TTetromino : public Tetromino {
+public:
+    TTetromino(); // Constructor khởi tạo khối T
+    ~TTetromino() override = default; // Destructor ảo để giải phóng bộ nhớ
+    void initializeShape() override; // Khởi tạo hình dạng khối T
+};
+
+#endif
