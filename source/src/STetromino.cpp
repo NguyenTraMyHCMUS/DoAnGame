ee896a0872b2fd5e1e249f017dffac1b6fedb0ee
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-04-29 12:53:12 +0700

    Update and rename SBlock.cpp to STetromino.cpp
    Sửa tên và toàn bộ code để phù hợp với Game quản lí

---
+++ source/src/STetromino.cpp
@@ -0,0 +1,14 @@
+#include "STetromino.h"
+
+STetromino::STetromino() {
+    _color = 3; // Màu sắc cho khối S
+    initializeShape(); // Khởi tạo hình dạng khối
+}
+
+void STetromino::initializeShape() {
+    int figures[4] = {3, 5, 4, 6}; // Hình dạng khối S
+    for (int i = 0; i < 4; i++) {
+        _blocks[i]._x = figures[i] % 2;
+        _blocks[i]._y = figures[i] / 2;
+    }
+}
