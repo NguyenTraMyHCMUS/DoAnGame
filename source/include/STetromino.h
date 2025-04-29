087c29c51e88561a68aac1c9a6fa776e1e92ae59
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-04-29 16:24:06 +0700

    Update STetromino.h
    bổ sung thêm phương thức đa hình rotate

--- source/include/STetromino.h
+++ source/include/STetromino.h
@@ -8,6 +8,7 @@ public:
     STetromino(); // Constructor khởi tạo khối S
     ~STetromino() override = default; // Destructor ảo để giải phóng bộ nhớ
     void initializeShape() override; // Khởi tạo hình dạng khối S
+    void rotate() override; // Xoay khối S (theo chiều kim đồng hồ)
 };
 
 #endif
