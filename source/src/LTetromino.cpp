63bcd6ff62a2f42f34f7b1a19a1d74711f41fea3
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-04-29 12:54:48 +0700

    Update and rename LBlock.cpp to LTetromino.cpp
    Sửa tên và toàn bộ code để phù hợp với Game quản lí

---
+++ source/src/LTetromino.cpp
@@ -0,0 +1,14 @@
+#include "LTetromino.h"
+
+LTetromino::LTetromino() {
+    _color = 5; // Màu sắc cho khối L
+    initializeShape(); // Khởi tạo hình dạng khối
+}
+
+void LTetromino::initializeShape() {
+    int figures[4] = {2, 3, 5, 7}; // Hình dạng khối L
+    for (int i = 0; i < 4; i++) {
+        _blocks[i]._x = figures[i] % 2;
+        _blocks[i]._y = figures[i] / 2;
+    }
+}
