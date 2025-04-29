00482cd2e1a667e90a666f23974ce0e8203ea706
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-04-29 16:22:24 +0700

    Update ITetromino.h
    bổ sung thêm phương thức đa hình rotate

--- source/include/ITetromino.h
+++ source/include/ITetromino.h
@@ -8,6 +8,7 @@ public:
     ITetromino();
     ~ITetromino() override = default; // Destructor ảo để giải phóng bộ nhớ
     void initializeShape() override; // Khởi tạo hình dạng khối I
+    void rotate() override; // Xoay khối I (ngang hoặc dọc)
 };
 
 #endif
