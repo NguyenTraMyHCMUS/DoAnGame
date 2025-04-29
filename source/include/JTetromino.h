1d78442fe7d57913db6e7c4b79feea25838efa65
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-04-29 13:02:50 +0700

    Update and rename JBlock.h to JTetromino.h

---
+++ source/include/JTetromino.h
@@ -0,0 +1,13 @@
+#ifndef JTETROMINO_H
+#define JTETROMINO_H
+
+#include "Tetromino.h"
+
+class JTetromino : public Tetromino {
+public:
+    JTetromino(); // Constructor khởi tạo khối J
+    ~JTetromino() override = default; // Destructor ảo để giải phóng bộ nhớ
+    void initializeShape() override; // Khởi tạo hình dạng khối
+};
+
+#endif
