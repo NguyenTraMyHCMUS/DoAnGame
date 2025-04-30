ecdfbf562975b4d4c582e6b723a44289b041fa60
NguyenKhaNhuHCMUS <23120311@student.hcmus.edu.vn>
2025-04-30 21:16:26 +0700

    Update and rename Level.cpp to LevelManager.cpp

---
+++ source/LevelManager.cpp
@@ -0,0 +1,24 @@
+#include "LevelManager.h"
+
+LevelManager::LevelManager() : currentLevel(1), linesCleared(0) {}
+
+void LevelManager::increaseLevel() {
+    currentLevel++;
+}
+
+void LevelManager::addClearedLines(int lines) {
+    linesCleared += lines;
+}
+
+int LevelManager::getLevel() const {
+    return currentLevel;
+}
+
+int LevelManager::getLinesCleared() const {
+    return linesCleared;
+}
+
+void LevelManager::reset() {
+    currentLevel = 1;
+    linesCleared = 0;
+}
