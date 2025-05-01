5f7be7297cc4f00389003b20c4965c0b3bcca76b
NguyenKhaNhuHCMUS <23120311@student.hcmus.edu.vn>
2025-05-01 15:23:06 +0700

    Update ScoreManager.h

--- source/ScoreManager.h
+++ source/ScoreManager.h
@@ -1,13 +1,20 @@
 #ifndef SCOREMANAGER_H
 #define SCOREMANAGER_H
 
+#include <string>
+
 class ScoreManager {
 private:
     int currentScore;
     int highScore;
+    const std::string highScoreFile = "highscore.txt"; // Tên tệp lưu điểm cao nhất
+
+    void loadHighScore(); // Hàm tải điểm cao nhất từ tệp
+    void saveHighScore() const; // Hàm lưu điểm cao nhất vào tệp
 
 public:
     ScoreManager();
+    ~ScoreManager(); // Lưu điểm cao nhất khi đối tượng bị hủy
     void addScore(int points);
     int getScore() const;
     int getHighScore() const;
