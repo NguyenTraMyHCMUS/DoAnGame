bc1696f89d1258762d10b1ae3d5d0366ad43faca
NguyenTraMyHCMUS <23120300@student.hcmus.edu.vn>
2025-05-02 11:25:16 +0700

    Update ITetromino.h

--- source/ITetromino.h
+++ source/ITetromino.h
@@ -9,9 +9,7 @@ public:
     ~ITetromino() override = default; // Destructor ảo để giải phóng bộ nhớ
     void initializeShape() override; // Khởi tạo hình dạng khối I
     void rotate() override; // Xoay khối I (ngang hoặc dọc)
-    std::unique_ptr<Tetromino> clone() const override {
-        return std::make_unique<ITetromino>(*this);
-    }
+    std::unique_ptr<Tetromino> clone() const override;
 };
 
 #endif
