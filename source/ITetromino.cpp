e4edce5fcd198389511887b1af2707c3a40ab375
NguyenTraMyHCMUS <23120300@student.hcmus.edu.vn>
2025-05-02 11:28:33 +0700

    Update ITetromino.cpp

--- source/ITetromino.cpp
+++ source/ITetromino.cpp
@@ -33,3 +33,7 @@ std::unique_ptr<Tetromino> ITetromino::clone() const
 {
     return std::make_unique<ITetromino>(*this);
 }
+std::unique_ptr<Tetromino> ITetromino::clone() const 
+{
+    return std::make_unique<ITetromino>(*this);
+}
