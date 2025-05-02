00e8be9190aec452b6988071fd317d4e028c3116
NguyenTraMyHCMUS <23120300@student.hcmus.edu.vn>
2025-05-02 11:25:35 +0700

    Update JTetromino.h

--- source/JTetromino.h
+++ source/JTetromino.h
@@ -9,6 +9,7 @@ public:
     ~JTetromino() override = default; // Destructor ảo để giải phóng bộ nhớ
     void initializeShape() override; // Khởi tạo hình dạng khối
     void rotate() override; // Xoay khối J (ngược chiều kim đồng hồ)
+    std::unique_ptr<Tetromino> clone() const override;
 };
 
 #endif
