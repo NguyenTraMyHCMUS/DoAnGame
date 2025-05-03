ff8083d12cc73c6662080ba880132b4d4d609c8b
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-05-03 10:16:15 +0700

    Update STetromino.h

--- source/STetromino.h
+++ source/STetromino.h
@@ -7,9 +7,10 @@ class STetromino : public Tetromino {
 public:
     STetromino(); // Constructor khởi tạo khối S
     ~STetromino() override = default; // Destructor ảo để giải phóng bộ nhớ
+
     void initializeShape() override; // Khởi tạo hình dạng khối S
     void rotate() override; // Xoay khối S (theo chiều kim đồng hồ)
-    std::unique_ptr<Tetromino> clone() const override;
+    std::unique_ptr<Tetromino> clone() const override; // Tạo một bản sao của khối S
 };
 
 #endif
