35168094f79e7ec6791e8a80a9d3088ad1ebec6e
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-05-03 10:12:57 +0700

    Update LTetromino.h

--- source/LTetromino.h
+++ source/LTetromino.h
@@ -7,9 +7,10 @@ class LTetromino : public Tetromino {
 public:
     LTetromino(); // Constructor khởi tạo khối L
     ~LTetromino() override = default; // Destructor ảo để giải phóng bộ nhớ
+
     void initializeShape() override; // Khởi tạo hình dạng khối L
     void rotate() override; // Xoay khối L (ngược chiều kim đồng hồ)
-    std::unique_ptr<Tetromino> clone() const override;
+    std::unique_ptr<Tetromino> clone() const override; // Tạo một bản sao của khối L
 };
 
 #endif
