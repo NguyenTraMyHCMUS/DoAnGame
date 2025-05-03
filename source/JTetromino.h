1212bcb277fa7ed4fef696a94d069da3e98e7362
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-05-03 10:11:29 +0700

    Update JTetromino.h

--- source/JTetromino.h
+++ source/JTetromino.h
@@ -7,9 +7,10 @@ class JTetromino : public Tetromino {
 public:
     JTetromino(); // Constructor khởi tạo khối J
     ~JTetromino() override = default; // Destructor ảo để giải phóng bộ nhớ
+
     void initializeShape() override; // Khởi tạo hình dạng khối
     void rotate() override; // Xoay khối J (ngược chiều kim đồng hồ)
-    std::unique_ptr<Tetromino> clone() const override;
+    std::unique_ptr<Tetromino> clone() const override; // Tạo một bản sao của khối J
 };
 
 #endif
