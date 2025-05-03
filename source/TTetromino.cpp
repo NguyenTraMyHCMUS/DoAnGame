f48764ddef92d0f7a2fbab6250af00cc43e61182
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-05-03 10:18:36 +0700

    Update TTetromino.cpp

--- source/TTetromino.cpp
+++ source/TTetromino.cpp
@@ -1,22 +1,26 @@
 #include "TTetromino.h"
 
+// Hàm khởi tạo khối T
 TTetromino::TTetromino() {
     _color = 4; // Màu sắc cho khối T
-    initializeShape(); // Khởi tạo hình dạng khối
+    initializeShape(); 
 }
 
+// Hàm khởi tạo hình dạng khối T
 void TTetromino::initializeShape() {
-    int figures[4] = {3, 5, 4, 7}; // Hình dạng khối T
+    int figures[4] = {3, 5, 4, 7}; 
     for (int i = 0; i < 4; i++) {
         _blocks[i]._x = figures[i] % 2;
         _blocks[i]._y = figures[i] / 2;
     }
 }
 
+// Hàm xoay khối T
 void TTetromino::rotate(){
     Tetromino::rotate(); // Gọi hàm xoay của lớp cha
 }
-std::unique_ptr<Tetromino> TTetromino::clone() const 
-{
-    return std::make_unique<TTetromino>(*this);
+
+// Hàm tạo một bản sao của khối T
+std::unique_ptr<Tetromino> TTetromino::clone() const {
+    return std::make_unique<TTetromino>(*this); 
 }
