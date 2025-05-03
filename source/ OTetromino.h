da14b9d2e67700132b401ddc36957b216cec5aea
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-05-03 10:14:09 +0700

    Update  OTetromino.h

--- source/ OTetromino.h
+++ source/ OTetromino.h
@@ -7,9 +7,10 @@ class OTetromino : public Tetromino {
 public:
     OTetromino(); // Constructor khởi tạo khối O
     ~OTetromino() override = default; // Destructor ảo để giải phóng bộ nhớ
+
     void initializeShape() override; // Khởi tạo hình dạng khối O
     void rotate() override; // Xoay khối O (không làm gì cả)
-    std::unique_ptr<Tetromino> clone() const override;
+    std::unique_ptr<Tetromino> clone() const override; // Tạo một bản sao của khối O
 };
 
 #endif
