7836c92a25b738e4f3db602a504dc44dc97bb5a0
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-05-03 10:12:31 +0700

    Update JTetromin.cpp

--- source/JTetromin.cpp
+++ source/JTetromin.cpp
@@ -1,10 +1,12 @@
 #include "JTetromino.h"
 
+// Hàm khởi tạo khối J
 JTetromino::JTetromino() {
     _color = 2; // Màu sắc cho khối J
     initializeShape(); // Khởi tạo hình dạng khối
 }
 
+// Hàm khởi tạo hình dạng khối J
 void JTetromino::initializeShape() {
     int figures[4] = {3, 5, 7, 6}; // Hình dạng khối J
     for (int i = 0; i < 4; i++) {
@@ -13,6 +15,7 @@ void JTetromino::initializeShape() {
     }
 }
 
+// Hàm xoay khối J
 void JTetromino::rotate() {
     Point center = _blocks[1]; // Điểm trung tâm để xoay
     for (int i = 0; i < 4; i++) {
@@ -23,7 +26,7 @@ void JTetromino::rotate() {
     }
 }
 
-std::unique_ptr<Tetromino> JTetromino::clone() const 
-{
+// Hàm tạo một bản sao của khối J
+std::unique_ptr<Tetromino> JTetromino::clone() const {
     return std::make_unique<JTetromino>(*this);
 }
