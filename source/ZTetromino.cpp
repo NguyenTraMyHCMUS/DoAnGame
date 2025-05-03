6d996ecba20fc800b5af15871c57a7a0013f1efe
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-05-03 10:19:47 +0700

    Update ZTetromino.cpp

--- source/ZTetromino.cpp
+++ source/ZTetromino.cpp
@@ -1,23 +1,26 @@
 #include "ZTetromino.h"
 
+// Hàm khởi tạo khối Z
 ZTetromino::ZTetromino() {
     _color = 7; // Màu sắc cho khối Z
-    initializeShape(); // Khởi tạo hình dạng khối
+    initializeShape(); 
 }
 
+// Hàm khởi tạo hình dạng khối Z
 void ZTetromino::initializeShape() {
-    int figures[4] = {2, 4, 5, 7}; // Hình dạng khối Z
+    int figures[4] = {2, 4, 5, 7}; 
     for (int i = 0; i < 4; i++) {
         _blocks[i]._x = figures[i] % 2;
         _blocks[i]._y = figures[i] / 2;
     }
 }
 
+// Hàm xoay khối Z
 void ZTetromino::rotate(){
     Tetromino::rotate(); // Gọi hàm xoay của lớp cha
 }
 
-std::unique_ptr<Tetromino> ZTetromino::clone() const 
-{
-    return std::make_unique<ZTetromino>(*this);
+// Hàm tạo một bản sao của khối Z
+std::unique_ptr<Tetromino> ZTetromino::clone() const {
+    return std::make_unique<ZTetromino>(*this); 
 }
