3a050b7bfa1929e0fd551c176ce569ca99a944cf
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-05-03 10:19:02 +0700

    Update ZTetromino.h

--- source/ZTetromino.h
+++ source/ZTetromino.h
@@ -7,9 +7,10 @@ class ZTetromino : public Tetromino {
 public:
     ZTetromino(); // Constructor khởi tạo khối Z
     ~ZTetromino() override = default; // Destructor ảo để giải phóng bộ nhớ
+    
     void initializeShape() override; // Khởi tạo hình dạng khối Z
     void rotate() override; // Xoay khối Z (theo chiều kim đồng hồ)
-     std::unique_ptr<Tetromino> clone() const override;
+    std::unique_ptr<Tetromino> clone() const override; // Tạo một bản sao của khối Z
 };
 
 #endif
