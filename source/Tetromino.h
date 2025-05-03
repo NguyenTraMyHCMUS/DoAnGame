4522d4cd6e4ad7bdfae4d2ada3e07a8a18987ff7
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-05-03 11:29:32 +0700

    Update Tetromino.h

--- source/Tetromino.h
+++ source/Tetromino.h
@@ -26,10 +26,11 @@ public:
     virtual void rotate(); // Xoay khối: O không xoay, I xoay ngang và dọc, {Z, T, S} xoay theo chiều kim đồng hồ, {L, J} ngược chiều kim đồng hồ
     virtual std::unique_ptr<Tetromino> clone() const = 0; //Tạo một bản sao của khối Tetris
 
-    int getColor() const; // Lấy màu sắc của khối
-    const Point* getBlocks() const;
-
     void setCellSize(int size); // Thay đổi kích thước ô trong khối
+    const Point* getBlocks() const; // Lấy danh sách các ô trong khối
+    void setColor(int color); // Thiết lập màu sắc cho khối
+    int getColor() const; // Lấy màu sắc của khối
+
     void backupState(); // Lưu trạng thái hiện tại của khối
     void restoreState(); // Khôi phục trạng thái từ backup
     void move(int dx); // Di chuyển khối theo chiều ngang
