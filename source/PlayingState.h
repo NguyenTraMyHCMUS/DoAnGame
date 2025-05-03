e3f5c23395321173873ca2a3ad6282e4ba26b3d0
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-05-03 11:43:27 +0700

    Update PlayingState.h

--- source/PlayingState.h
+++ source/PlayingState.h
@@ -8,16 +8,14 @@ class Game;
 
 class PlayingState : public GameState {
 private:
-    Game& _game;
+    Game& _game; // Tham chiếu đến đối tượng Game
     sf::Font _font; // Font cho chữ
-    // sf::Texture _playingTexture; // Texture cho hình ảnh chơi
-    // sf::Sprite _playingSprite;   // Sprite để hiển thị hình ảnh chơi
 
 public:
-    PlayingState(Game& game);
-    void handleInput(Game& game) override;
-    void drawInfoBox(sf::RenderWindow& window, sf::Vector2f position, const std::string& title, const std::string& value);
-    void draw(Game& game) override;
+    PlayingState(Game& game); // Constructor
+    void handleInput(Game& game) override; // Xử lý đầu vào (tải hình ảnh, font chữ, v.v.)
+    void drawInfoBox(sf::RenderWindow& window, sf::Vector2f position, const std::string& title, const std::string& value); // Vẽ hộp thông tin chứa điểm số, level, khối tiếp theo
+    void draw(Game& game) override; // Vẽ toàn cảnh trạng thái playing 
 };
 
 #endif 
