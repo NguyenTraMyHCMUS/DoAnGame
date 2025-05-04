864e10c5b091ef2249ea647420a26453075c9285
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-05-05 00:36:10 +0700

    Update GameOverState.h

--- source/GameOverState.h
+++ source/GameOverState.h
@@ -1,3 +1,11 @@
+/**
+ * @file GameOverState.h
+ * @brief Định nghĩa lớp GameOverState cho trạng thái "Game Over" của trò chơi.
+ * 
+ * Lớp này kế thừa từ GameState, cung cấp các phương thức và thuộc tính cần thiết để quản lý giao diện "Game Over",
+ * bao gồm việc xử lý đầu vào từ người dùng và vẽ các thành phần giao diện lên cửa sổ game.
+ */
+
 #ifndef GAMEOVERSTATE_H
 #define GAMEOVERSTATE_H
 
@@ -31,8 +39,8 @@ public:
     /**
      * @brief Constructor.
      * 
-     * Khởi tạo trạng thái "GameOver", bao gồm việc tải tài nguyên,
-     * thiết lập hình ảnh nền và các văn bản liên quan.
+     * Khởi tạo trạng thái "GameOver" bao gồm: tải các tài nguyên (hình ảnh, phông chữ), khởi tạo và thiết lập các thành phần của menu,
+     * thiết lập hình ảnh nền và các văn bản liên quan (màu sắc, kích thước, vị trí).
      * 
      * @param game Tham chiếu đến đối tượng Game chính.
      */
@@ -41,12 +49,13 @@ public:
     /**
      * @brief Xử lý đầu vào từ người dùng (bàn phím, chuột).
      * 
+     * @details
      * Hỗ trợ người dùng tương tác với menu "Game Over": 
-     * Nháy chuột trái vào nút X ở góc trên bên trái để đóng cửa sổ.
-     * Di chuyển giữa các mục menu bằng keyboard (mũi tên lên/xuống),
+     * - Nháy chuột trái vào nút X ở góc trên bên trái để đóng cửa sổ.
+     * - Di chuyển giữa các mục menu bằng keyboard (mũi tên lên/xuống),
      * thực hiện chọn một mục bằng cách nhấn Enter.
-     * Chọn "Retry": để chuyển sang trạng thái chơi game.
-     * Chọn "Exit": để thoát trò chơi.
+     *   + Chọn "Retry": để chuyển sang trạng thái chơi game.
+     *   + Chọn "Exit": để thoát trò chơi.
      * 
      * @param game Tham chiếu đến đối tượng Game chính.
      */
@@ -55,7 +64,7 @@ public:
     /**
      * @brief Vẽ màn hình "Game Over" và menu lựa chọn lên cửa sổ.
      * 
-     * Bao gồm nền, tiêu đề, và các mục menu.
+     * Bao gồm hình nền, tiêu đề, và các mục menu.
      * 
      * @param game Tham chiếu đến đối tượng Game chính.
      */
