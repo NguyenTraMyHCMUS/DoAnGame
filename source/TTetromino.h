da1dd52d274f82ca96081a52af29cc189df13b83
NguyenTraMyHCMUS <23120300@student.hcmus.edu.vn>
2025-05-04 20:56:19 +0700

    Update TTetromino.h

--- source/TTetromino.h
+++ source/TTetromino.h
@@ -3,14 +3,50 @@
 
 #include "Tetromino.h"
 
+/**
+ * @class TTetromino
+ * @brief Lớp đại diện cho khối T trong trò chơi Tetris.
+ * 
+ * Khối T có hình dạng chữ T và có khả năng xoay theo chiều kim đồng hồ. Lớp này kế thừa từ lớp `Tetromino` và cài đặt các phương thức đặc trưng cho khối T.
+ */
 class TTetromino : public Tetromino {
 public:
-    TTetromino(); // Constructor khởi tạo khối T
-    ~TTetromino() override = default; // Destructor ảo để giải phóng bộ nhớ
+    /**
+     * @brief Constructor khởi tạo khối T.
+     * 
+     * Khởi tạo các thuộc tính của khối T, bao gồm kích thước ô và màu sắc.
+     */
+    TTetromino();
 
-    void initializeShape() override; // Khởi tạo hình dạng khối T
-    void rotate() override; // Xoay khối T (theo chiều kim đồng hồ)
-    std::unique_ptr<Tetromino> clone() const override; // Tạo một bản sao của khối T
+    /**
+     * @brief Destructor ảo để giải phóng bộ nhớ.
+     * 
+     * Phương thức này sẽ giải phóng bộ nhớ khi đối tượng khối T bị hủy.
+     */
+    ~TTetromino() override = default;
+
+    /**
+     * @brief Khởi tạo hình dạng khối T.
+     * 
+     * Phương thức này cài đặt hình dạng ban đầu của khối T (các ô được sắp xếp theo hình chữ T).
+     */
+    void initializeShape() override;
+
+    /**
+     * @brief Xoay khối T theo chiều kim đồng hồ.
+     * 
+     * Phương thức này xoay khối T theo chiều kim đồng hồ khi người chơi thực hiện thao tác xoay.
+     */
+    void rotate() override;
+
+    /**
+     * @brief Tạo một bản sao của khối T.
+     * 
+     * Phương thức này tạo một bản sao của khối T và trả về một con trỏ duy nhất đến đối tượng bản sao.
+     * 
+     * @return Một con trỏ duy nhất đến bản sao của khối T.
+     */
+    std::unique_ptr<Tetromino> clone() const override;
 };
 
 #endif
