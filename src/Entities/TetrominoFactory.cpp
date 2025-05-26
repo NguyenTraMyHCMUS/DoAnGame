26a057eb660510d55287b8b0ef4fcf4064917d07
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-05-26 16:14:24 +0700

    Update TetrominoFactory.cpp

--- src/Entities/TetrominoFactory.cpp
+++ src/Entities/TetrominoFactory.cpp
@@ -1,4 +1,5 @@
 #include "TetrominoFactory.h"
+#include "RotatorFactory.h"
 
 std::unique_ptr<Tetromino> TetrominoFactory::createRandomTetromino() {
     int type = rand() % 7; // Chọn ngẫu nhiên một loại khối
@@ -13,14 +14,47 @@ std::unique_ptr<Tetromino> TetrominoFactory::createRandomTetromino() {
 }
 
 std::unique_ptr<Tetromino> TetrominoFactory::createTetromino(int type) {
+    std::unique_ptr<Tetromino> tetromino;
+    std::string rotatorType;
+
+    // Tạo đúng loại tetromino dựa vào type
     switch (type) {
-        case 0: return std::make_unique<ITetromino>();
-        case 1: return std::make_unique<JTetromino>();
-        case 2: return std::make_unique<LTetromino>();
-        case 3: return std::make_unique<OTetromino>();
-        case 4: return std::make_unique<STetromino>();
-        case 5: return std::make_unique<TTetromino>();
-        case 6: return std::make_unique<ZTetromino>();
-        default: return nullptr;
+        case 0: 
+            tetromino = std::make_unique<ITetromino>();
+            rotatorType = "I";
+            break;
+        case 1: 
+            tetromino = std::make_unique<JTetromino>();
+            rotatorType = "J";
+            break;
+        case 2: 
+            tetromino = std::make_unique<LTetromino>();
+            rotatorType = "L";
+            break;
+        case 3: 
+            tetromino = std::make_unique<OTetromino>();
+            rotatorType = "O";
+            break;
+        case 4: 
+            tetromino = std::make_unique<STetromino>();
+            rotatorType = "S";
+            break;
+        case 5: 
+            tetromino = std::make_unique<TTetromino>();
+            rotatorType = "T";
+            break;
+        case 6: 
+            tetromino = std::make_unique<ZTetromino>();
+            rotatorType = "Z";
+            break;
+        default: 
+            return nullptr;
     }
+    
+    // Gán rotator phù hợp
+    if (rotatorType != "O") {  // Khối O không cần rotator
+        tetromino->setRotator(RotatorFactory::createRotator(rotatorType));
+    }
+    
+    return tetromino;
 }
