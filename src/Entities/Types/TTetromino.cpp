f3a75e783353139d64b37e9899fdf7da02d84fd3
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-05-27 10:03:31 +0700

    Update TTetromino.cpp

--- src/Entities/Types/TTetromino.cpp
+++ src/Entities/Types/TTetromino.cpp
@@ -1,11 +1,30 @@
 #include "TTetromino.h"
-#include "../RotatorFactory.h"
+
+#include "../Factories/RotatorFactory.h"
+#include "../Registries/TetrominoAutoRegistrar.h"
+#include "../Configuration/ColorMapper.h"
+
+// Đăng ký khối O với ID 5 và tên "T"
+REGISTER_TETROMINO(TTetromino, 5, "T")
+
+// Hàm khởi tạo khối T với thành phần từ factory
+TTetromino::TTetromino(ITetrominoComponentFactory& factory) : Tetromino(factory) {
+    _color = ColorMapper::getInstance().getColor("T"); // Màu sắc cho khối T
+    initializeShape(); // Khởi tạo hình dạng khối
+    setupRotator(); // Tạo rotator cho khối T
+}
 
 // Hàm khởi tạo khối T
 TTetromino::TTetromino() {
-    _color = 4; // Màu sắc cho khối T
+    _color = ColorMapper::getInstance().getColor("T"); // Màu sắc cho khối T
     initializeShape(); 
-   _rotator = new StandardRotator();
+   setupRotator(); // Tạo rotator cho khối L
+}
+
+void TTetromino::setupRotator() {
+    if (_componentFactory) {
+        _rotator = _componentFactory->createRotator("T");
+    }
 }
 
 // Hàm khởi tạo hình dạng khối T
@@ -20,20 +39,22 @@ void TTetromino::initializeShape() {
 // Hàm xoay khối T
 void TTetromino::rotate(){
     if (_rotator) {
-        _rotator->rotate(_blocks); // Sử dụng con trỏ với ->
+        _rotator->rotate(_blocks); 
     }
 }
 
 // Hàm tạo một bản sao của khối T
 std::unique_ptr<Tetromino> TTetromino::clone() const {
     auto copy = std::make_unique<TTetromino>();
+
     copy->setColor(_color);
-    
     copy->cloneComponents(*this);
-    
-    // Đảm bảo khởi tạo rotator cho bản sao
-     copy->setRotator(RotatorFactory::createRotator("T"));
+    copy->setRotator(RotatorFactory::createRotator("T"));
     
     return copy;
+}
+
+std::string TTetromino::getTypeName() const { 
+    return "T"; // Trả về tên loại khối T
 }
 
