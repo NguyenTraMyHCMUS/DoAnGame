cf87fd2f9484b4aee0de709c29423317a454e9df
NguyenKhaNhuHCMUS <23120311@student.hcmus.edu.vn>
2025-05-27 21:46:22 +0700

    Update ZTetromino.cpp

--- src/Entities/Types/ZTetromino.cpp
+++ src/Entities/Types/ZTetromino.cpp
@@ -29,8 +29,9 @@ void ZTetromino::setupRotator() {
 // Hàm khởi tạo hình dạng khối Z
 void ZTetromino::initializeShape() {
     int figures[4] = {2, 4, 5, 7}; 
+    int offsetX = rand() % (N - 3);
     for (int i = 0; i < 4; i++) {
-        _blocks[i].setX(figures[i] % 2);
+        _blocks[i].setX(figures[i] % 2 + offsetX);
         _blocks[i].setY(figures[i] / 2);
     }
 }
