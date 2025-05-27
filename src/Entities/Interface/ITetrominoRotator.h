6aff68101426e4fffa83339277eaa0ec1d4724a4
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2025-05-27 09:46:51 +0700

    Update ITetrominoRotator.h

--- src/Entities/Interface/ITetrominoRotator.h
+++ src/Entities/Interface/ITetrominoRotator.h
@@ -3,10 +3,32 @@
 
 #include "../Point.h"
 
+/**
+ * @file ITetrominoRotator.h
+ * @brief Định nghĩa interface cho chức năng xoay khối Tetromino trong game Tetris.
+ */
+
+/**
+ * @class ITetrominoRotator
+ * @brief Interface chuyên trách việc xoay khối Tetromino.
+ *
+ * Tuân thủ nguyên lý **Interface Segregation Principle (ISP)** trong SOLID:
+ * - Chỉ tập trung vào chức năng xoay, không phụ thuộc vào logic khác như di chuyển hay hiển thị.
+ */
 class ITetrominoRotator {
 public:
+    /**
+     * @brief Hàm hủy ảo để hỗ trợ đa hình và đảm bảo hủy tài nguyên đúng cách.
+     */
     virtual ~ITetrominoRotator() = default;
+
+    /**
+     * @brief Xoay khối Tetromino bằng cách cập nhật các điểm tọa độ của các ô.
+     *
+     * @param blocks Mảng gồm 4 điểm đại diện cho vị trí các ô của khối Tetromino.
+     *               Sau khi thực hiện, mảng sẽ chứa các vị trí mới sau khi xoay.
+     */
     virtual void rotate(Point (&blocks)[4]) = 0;
 };
 
-#endif
+#endif // ITETROMINO_ROTATOR_H
