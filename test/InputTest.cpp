808dc33a1532c47557820630d6493a646374e30b
NguyenKhaNhuHCMUS <23120311@student.hcmus.edu.vn>
2025-05-31 14:37:32 +0700

    Update InputTest.cpp

--- test/InputTest.cpp
+++ test/InputTest.cpp
@@ -1,7 +1,19 @@
 #include "catch.hpp"
 #include "../Input/InputManager.h"
-#include "../Input/InputHandler.h"
+#include "../Input/IInputHandler.h"
 #include "../Input/InputPlayerName.h"
+
+class DummyInputHandler : public IInputHandler {
+    int dx = 0;
+    bool rotate = false;
+public:
+    void handleInput(Game&) override {}
+    int getDx() const override { return dx; }
+    bool getRotate() const override { return rotate; }
+    void reset() override { dx = 0; rotate = false; }
+    void setDx(int value) override { dx = value; }
+    void setRotate(bool value) override { rotate = value; }
+};
 
 // InputManager tests
 TEST_CASE("InputManager default values", "[InputManager]") {
@@ -27,15 +39,25 @@ TEST_CASE("InputManager reset", "[InputManager]") {
     REQUIRE(im.getRotate() == false);
 }
 
-// InputHandler tests
+// Có thể test processInput nếu muốn (ví dụ với sự kiện giả lập)
+// TEST_CASE("InputManager processInput", "[InputManager]") {
+//     InputManager im;
+//     sf::Event event;
+//     event.type = sf::Event::KeyPressed;
+//     event.key.code = sf::Keyboard::Left;
+//     im.processInput(event);
+//     // Kiểm tra dx thay đổi nếu bạn implement logic
+// }
+
+// InputHandler tests (nếu là class riêng biệt)
 TEST_CASE("InputHandler default values", "[InputHandler]") {
-    InputHandler ih;
+    DummyInputHandler ih;
     REQUIRE(ih.getDx() == 0);
     REQUIRE(ih.getRotate() == false);
 }
 
 TEST_CASE("InputHandler set/get dx and rotate", "[InputHandler]") {
-    InputHandler ih;
+    DummyInputHandler ih;
     ih.setDx(-1);
     ih.setRotate(true);
     REQUIRE(ih.getDx() == -1);
@@ -43,7 +65,7 @@ TEST_CASE("InputHandler set/get dx and rotate", "[InputHandler]") {
 }
 
 TEST_CASE("InputHandler reset", "[InputHandler]") {
-    InputHandler ih;
+    DummyInputHandler ih;
     ih.setDx(3);
     ih.setRotate(true);
     ih.reset();
@@ -51,7 +73,7 @@ TEST_CASE("InputHandler reset", "[InputHandler]") {
     REQUIRE(ih.getRotate() == false);
 }
 
-// PlayerNameInput: Chỉ test được constructor và interface vì phụ thuộc vào window thực
+// PlayerNameInput chỉ test được constructor
 TEST_CASE("PlayerNameInput can be constructed", "[PlayerNameInput]") {
     sf::RenderWindow window(sf::VideoMode(100, 100), "Test");
     PlayerNameInput input(window);
