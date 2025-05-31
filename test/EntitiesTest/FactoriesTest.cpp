dc877d2868c9198ccde3a2d7d10540dc6c3fed68
NguyenKhaNhuHCMUS <23120311@student.hcmus.edu.vn>
2025-05-31 19:53:31 +0700

    Update FactoriesTest.cpp

--- test/EntitiesTest/FactoriesTest.cpp
+++ test/EntitiesTest/FactoriesTest.cpp
@@ -5,10 +5,9 @@
 #include "../../Entities/Point.h"
 
 TEST_CASE("TetrominoFactory createTetromino by id and name", "[TetrominoFactory]") {
-    
     for (int i = 0; i < TetrominoFactory::getAvailableTypeCount(); ++i) {
-        auto tet = TetrominoFactory::createTetromino(i);
-        REQUIRE(tet != nullptr);
+        auto tetromino = TetrominoFactory::createTetromino(i);
+        REQUIRE(tetromino != nullptr);
         REQUIRE(TetrominoFactory::isValidType(i));
     }
     REQUIRE_FALSE(TetrominoFactory::isValidType(-1));
@@ -16,16 +15,16 @@ TEST_CASE("TetrominoFactory createTetromino by id and name", "[TetrominoFactory]
 
     std::vector<std::string> names = {"I", "J", "L", "O", "S", "T", "Z"};
     for (const auto& name : names) {
-        auto tet = TetrominoFactory::createTetromino(name);
-        REQUIRE(tet != nullptr);
+        auto tetromino = TetrominoFactory::createTetromino(name);
+        REQUIRE(tetromino != nullptr);
         REQUIRE(TetrominoFactory::isValidName(name));
     }
     REQUIRE_FALSE(TetrominoFactory::isValidName("X"));
 }
 
 TEST_CASE("TetrominoFactory createRandomTetromino", "[TetrominoFactory]") {
-    auto tet = TetrominoFactory::createRandomTetromino();
-    REQUIRE(tet != nullptr);
+    auto tetromino = TetrominoFactory::createRandomTetromino();
+    REQUIRE(tetromino != nullptr);
 }
 
 TEST_CASE("TetrominoFactory getTypeInfo and getAllTypeInfo", "[TetrominoFactory]") {
@@ -34,33 +33,33 @@ TEST_CASE("TetrominoFactory getTypeInfo and getAllTypeInfo", "[TetrominoFactory]
     for (size_t i = 0; i < infos.size(); ++i) {
         auto info = TetrominoFactory::getTypeInfo(static_cast<int>(i));
         REQUIRE(info != nullptr);
-        auto infoByName = TetrominoFactory::getTypeInfo(infos[i].name);
-        REQUIRE(infoByName != nullptr);
+        auto info_by_name = TetrominoFactory::getTypeInfo(infos[i].name);
+        REQUIRE(info_by_name != nullptr);
     }
     REQUIRE(TetrominoFactory::getTypeInfo(-1) == nullptr);
     REQUIRE(TetrominoFactory::getTypeInfo("X") == nullptr);
 }
 
 TEST_CASE("TetrominoFactory createTetrominoWithMetadata", "[TetrominoFactory]") {
-    auto tet = TetrominoFactory::createTetrominoWithMetadata(0);
-    REQUIRE(tet != nullptr);
-    tet = TetrominoFactory::createTetrominoWithMetadata("I");
-    REQUIRE(tet != nullptr);
+    auto tetromino = TetrominoFactory::createTetrominoWithMetadata(0);
+    REQUIRE(tetromino != nullptr);
+    tetromino = TetrominoFactory::createTetrominoWithMetadata("I");
+    REQUIRE(tetromino != nullptr);
 }
 
 TEST_CASE("RotatorFactory createRotator returns not null for valid types", "[RotatorFactory]") {
     std::vector<std::string> names = {"I", "J", "L", "S", "T", "Z"};
     for (const auto& name : names) {
-        auto rot = RotatorFactory::createRotator(name);
+        auto rotator = RotatorFactory::createRotator(name);
         INFO("Type: " << name);
-        REQUIRE(rot != nullptr);
+        REQUIRE(rotator != nullptr);
     }
     // O không cần rotator, phải trả về nullptr
-    auto rotO = RotatorFactory::createRotator("O");
-    REQUIRE(rotO == nullptr);
+    auto rotator_o = RotatorFactory::createRotator("O");
+    REQUIRE(rotator_o == nullptr);
 
-    auto rot = RotatorFactory::createRotator("X");
-    REQUIRE(rot == nullptr);
+    auto rotator = RotatorFactory::createRotator("X");
+    REQUIRE(rotator == nullptr);
 }
 
 TEST_CASE("DefaultTetrominoComponentFactory create all components", "[DefaultTetrominoComponentFactory]") {
