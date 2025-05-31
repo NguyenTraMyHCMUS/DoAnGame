fd492ffc3b182fbbe1fc50ac7bf10ed44ba6b7e9
NguyenKhaNhuHCMUS <23120311@student.hcmus.edu.vn>
2025-05-31 19:52:16 +0700

    Update ConfigurationTest.cpp

--- test/EntitiesTest/ConfigurationTest.cpp
+++ test/EntitiesTest/ConfigurationTest.cpp
@@ -3,30 +3,30 @@
 #include "../../Entities/Configuration/ColorMapper.h"
 
 TEST_CASE("ConfigurationManager singleton and int/string set/get", "[ConfigurationManager]") {
-    ConfigurationManager& config = ConfigurationManager::getInstance();
+    ConfigurationManager& config_manager = ConfigurationManager::getInstance();
 
-    config.setInt("test_int", 123);
-    REQUIRE(config.getInt("test_int") == 123);
-    REQUIRE(config.getInt("not_exist", 42) == 42);
+    config_manager.setInt("test_int", 123);
+    REQUIRE(config_manager.getInt("test_int") == 123);
+    REQUIRE(config_manager.getInt("not_exist", 42) == 42);
 
-    config.setString("test_str", "hello");
-    REQUIRE(config.getString("test_str") == "hello");
-    REQUIRE(config.getString("not_exist", "default") == "default");
+    config_manager.setString("test_str", "hello");
+    REQUIRE(config_manager.getString("test_str") == "hello");
+    REQUIRE(config_manager.getString("not_exist", "default") == "default");
 }
 
 TEST_CASE("ColorMapper singleton and color mapping", "[ColorMapper]") {
-    ColorMapper& mapper = ColorMapper::getInstance();
+    ColorMapper& color_mapper = ColorMapper::getInstance();
 
     // Thiết lập và kiểm tra màu cho type "I"
-    mapper.setColor("I", 5);
-    REQUIRE(mapper.getColor("I") == 5);
-    REQUIRE(mapper.hasColor("I"));
+    color_mapper.setColor("I", 5);
+    REQUIRE(color_mapper.getColor("I") == 5);
+    REQUIRE(color_mapper.hasColor("I"));
 
     // Kiểm tra type không tồn tại
-    REQUIRE_FALSE(mapper.hasColor("XYZ"));
+    REQUIRE_FALSE(color_mapper.hasColor("XYZ"));
 }
 
 TEST_CASE("ColorMapper updateFromConfig does not throw", "[ColorMapper]") {
-    ColorMapper& mapper = ColorMapper::getInstance();
-    REQUIRE_NOTHROW(mapper.updateFromConfig());
+    ColorMapper& color_mapper = ColorMapper::getInstance();
+    REQUIRE_NOTHROW(color_mapper.updateFromConfig());
 }
