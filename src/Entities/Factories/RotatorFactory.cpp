c2ce69ee6601defb550f391dce55a7924f1cceb1
NguyenTraMyHCMUS <23120300@student.hcmus.edu.vn>
2025-08-02 19:02:22 +0700

    delete all files

--- src/Entities/Factories/RotatorFactory.cpp
+++
@@ -1,7 +0,0 @@
-#include "RotatorFactory.h"
-
-// Hàm tạo rotator cho một loại khối Tetromino
-std::unique_ptr<ITetrominoRotator> RotatorFactory::createRotator(const std::string& type) {
-    auto& registry = RotatorFactoryRegistry::getInstance();
-    return registry.createRotator(type);
-}
